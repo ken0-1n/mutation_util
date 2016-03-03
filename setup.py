5ecd634e5c64c30206996975bee90371aadf043c
Kenichi Chiba <kchiba@hgc.jp>
2016-03-03 17:20:16 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mutation-util',
+    version='0.1.0',
+    description='Python tools to identify somatic mutations.',
+    author='Ken-ichi Chiba',
+    author_email='kchiba@hgc.jp',
+    url='https://github.com/Genomon-Project/GenomonFisher',
+    package_dir = {'': 'lib'},
+    packages=['mutil'],
+    scripts=['mutil'],
+    license='GPL-3'
+)
