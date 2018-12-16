fef9035bca53822d578b3e88c39e768b1d663e42
EC2 Default User <ec2-user@ip-172-31-39-78.us-west-2.compute.internal>
2018-12-16 04:10:40 +0000

    modify hotspot_merge.py

--- lib/mutil/hotspot_merge.py
+++ lib/mutil/hotspot_merge.py
@@ -7,8 +7,6 @@ def merge_hotspot_list(in_hotspot_mutation, in_genomon_mutation, output_file, sk
     is_header = skip_h_header
     with open(in_hotspot_mutation, 'r') as hin:
         for line in hin:
-            # skip meta data
-            if line.startswith("#"): continue
             # skip header line
             if is_header:
                 is_header = False
@@ -26,13 +24,12 @@ def merge_hotspot_list(in_hotspot_mutation, in_genomon_mutation, output_file, sk
     is_header = skip_g_header
     with open(in_genomon_mutation, 'r') as hin:
         for line in hin:
-            # skip meta data
-            if line.startswith("#"): continue
+            line = line.rstrip('\n')
             # skip header line
             if is_header:
                 is_header = False
+                print >> hout, line +"\tscore(hotspot)"
                 continue
-            line = line.rstrip('\n')
             F = line.split('\t')
             chr = F[0]
             start = F[1]
