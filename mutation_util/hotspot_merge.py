8be2a78528ccb89242646da40d8a6957cf544602
Ubuntu <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2022-04-08 10:38:19 +0000

    support bcftools-v1.14.0

--- mutation_util/hotspot_merge.py
+++ mutation_util/hotspot_merge.py
@@ -5,7 +5,6 @@ def merge_hotspot_vcf(in_vcf, hotspot_vcf, merged_vcf):
     out_prefix, ext = os.path.splitext(merged_vcf)
     subprocess.check_call(['bcftools', 'isec', '-p', out_prefix, in_vcf, hotspot_vcf]) 
 
-
     with open(out_prefix+'_sorted.vcf', 'w') as hout:
         with gzip.open(in_vcf, 'rt') as hin:
             for line in hin:
@@ -25,12 +24,13 @@ def merge_hotspot_vcf(in_vcf, hotspot_vcf, merged_vcf):
                 line = line.rstrip('\n')
                 print(line, file=hout)
 
-    subprocess.check_call(['bcftools', 'sort', '-O', 'vcf', '-o', merged_vcf, out_prefix+'_sorted.vcf']) 
+    subprocess.check_call(['bcftools', 'sort', '-O', 'v', '-o', merged_vcf, out_prefix+'_sorted.vcf']) 
     os.remove(out_prefix+'/0000.vcf')
     os.remove(out_prefix+'/0001.vcf')
     os.remove(out_prefix+'/0002.vcf')
     os.remove(out_prefix+'/0003.vcf')
     os.remove(out_prefix+'/README.txt')
+    os.remove(out_prefix+'/sites.txt')
     os.rmdir(out_prefix)
     os.remove(out_prefix+'_sorted.vcf')
 
